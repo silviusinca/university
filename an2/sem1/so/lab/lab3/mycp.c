e0a6f13af392b0f16422ca491f07389472fed081
gabrielsinca <gabrielsinca14@gmail.com>
2022-10-25 10:43:12 +0300

    add asc

--- an2/sem1/so/lab/lab3/mycp.c
+++ an2/sem1/so/lab/lab3/mycp.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv){
+	
 	size_t bytes = 100;
 	char* buffer = (char*)malloc(bytes * sizeof(char));
 	int fd = open(argv[1], O_RDONLY);	
 
 	int fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
 
-	ssize_t nread = read(fd, buffer, bytes);
-	write(fd2, buffer, nread);
-
+	size_t nread = read(fd, buffer, bytes);
+	while (nread > 0) {
+		write(fd2, buffer, nread);
+		nread = read(fd, buffer, bytes);
+	}
 	return 0;
 }
